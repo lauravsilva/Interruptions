9fc1c958b7078d259578a8fc26e8404bfe5d523b
lauravsilva <lvs1646@rit.edu>
2017-02-14 14:58:11 -0500

    Here are the actual files

---
+++ print.js
@@ -0,0 +1,12 @@
+var createConfig = require('./config');
+var createFileRenderer = require('./lib/createFileRenderer');
+
+console.log(process.argv[0], process.argv[1]);
+
+// do all that jazz
+var seed = typeof process.argv[2] !== 'undefined'
+  ? String(process.argv[2])
+  : undefined;
+var config = createConfig(seed);
+
+createFileRenderer(config);
