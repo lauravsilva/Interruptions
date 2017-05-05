5f4be59191b30f45e79035fe5d3216e5cdfa35b0
lauravsilva <lvs1646@rit.edu>
2017-05-05 19:45:33 -0400

    slight adjustments to survey slider js

--- assets/js/survey_main.js
+++ assets/js/survey_main.js
@@ -1,56 +1,89 @@
 var result = "";
-var resValue = 50;
+var resValue = "";
+var percentValue = "";
+
 var option1 = document.getElementById("op0").textContent;
 var option2 = document.getElementById("op1").textContent;
 var option3 = document.getElementById("op2").textContent;
 var option4 = document.getElementById("op3").textContent;
 var option5 = document.getElementById("op4").textContent;
 
-var halfHandleHeight = 60;
+//TODO: change handle height to dynamic
+var halfHandleHeight = $('#drag-handle').height()/2;
+var sliderheightDynamic = 400;
+var sliderSectionsHeight = 0;
+var headerHeight;
+
+//Prepare the slider
+// var range = sliderheightDynamic
+//     , sliderDiv = $("#slider")
+//     , position = sliderDiv.position()
+//     , minY = position.top
+//     , maxY = minY + sliderheightDynamic
+//     , tickSize = sliderheightDynamic / range;
+
+
+
 
 $(document).ready(function(){
     // $("#up-arrow").effect("bounce", "slow");
 
-    var headerHeight = $("#header").height();
-    console.log("header: " + headerHeight);
+    headerHeight = $("#header").height();
+    console.log("header " + headerHeight);
 
-    var heightToSet = $(window).height() - headerHeight;
+    // sliderheightDynamic = $(window).height() - headerHeight;
+    sliderheightDynamic = $(window).height() - headerHeight;
     console.log("$(window).height() " + $(window).height());
-    console.log("heightToSet " + heightToSet);
+    console.log("sliderheightDynamic " + sliderheightDynamic);
 
-    $("#slider").css("height",heightToSet + "px");
+    sliderSectionsHeight = sliderheightDynamic/5;
+    console.log("sliderSectionsHeight " + sliderSectionsHeight);
+
+    console.log("halfHandleHeight " + halfHandleHeight);
+
+    $("#slider").css("height",sliderheightDynamic);
+
+
+
+    console.log("input " + input);
+    console.log("low " + low);
+    console.log("high " + high);
+
+
+    console.log("slider offset " + $("#slider").offset().top);
 
 
 });
 
 
-//Prepare the slider
-var range = 100
-    , sliderDiv = $("#slider");
+var sliderDiv = $("#slider")
+    , input = sliderheightDynamic
+    , low = $("#header").height() + halfHandleHeight + 5
+    , high = $(window).height() - halfHandleHeight - 20
+    , min = 0
+    , max = 99;
+
+
 
 // Activate the UI slider
 sliderDiv.slider({
     orientation: "vertical"
-    , min: 0
-    , max: 100
-    , step: 25
-    , value: 50
+    , min: min
+    , max: max
+    // , step: step
+    // , value: 50
     , create: function () {
         $(this).find(".ui-slider-handle").hide();
     }
 });
-var position = sliderDiv.position()
-    , sliderHeight = sliderDiv.height()
-    , minY = position.top
-    , maxY = minY + sliderHeight
-    , tickSize = sliderHeight / range;
+
+
 
 //the draggable object
 $("#drag-handle").draggable({
     containment: "parent",
     axis: "y"
     , drag: function (e, ui) {
-
         $("#drag-handle").css("background", "#020b11");
         $("#drag-handle").css("opacity", "0.7");
 
@@ -61,25 +94,40 @@ $("#drag-handle").draggable({
         }
 
         var offset = $(this).offset();
-        var containerTop = offset.top;
-        resValue = Math.round((containerTop - minY + halfHandleHeight) / tickSize);
+        var handleToTop = offset.top;
 
-        sliderDiv.slider("value", resValue);
+        resValue = Math.ceil(handleToTop + halfHandleHeight + halfHandleHeight);
+        percentValue = linear(resValue, low, high, min, max);
+
+        if (percentValue < 0){
+            percentValue = 0
+        }
+        else if (percentValue > 99){
+            percentValue = 99
+        }
+
+
+        console.log("container top: " + handleToTop);
+        console.log("resValue: " + resValue);
+        console.log("percentValue " + percentValue);
+
+
+        sliderDiv.slider("value", percentValue);
 
         //TODO: adjust these numbers to make more sense
-        if (resValue <= 30) {
+        if (percentValue <= 20) {
             result = option1;
         }
-        else if (resValue >= 21 && resValue <= 50) {
+        else if (percentValue >= 21 && percentValue <= 50) {
             result = option2;
         }
-        else if (resValue >= 51 && resValue <= 60) {
+        else if (percentValue >= 51 && percentValue <= 60) {
             result = option3;
         }
-        else if (resValue >= 61 && resValue <= 80) {
+        else if (percentValue >= 61 && percentValue <= 80) {
             result = option4;
         }
-        else if (resValue >= 81 && resValue <= 100) {
+        else if (percentValue >= 81 && percentValue <= 100) {
             result = option5;
         }
 
@@ -88,49 +136,50 @@ $("#drag-handle").draggable({
 });
 
 //Set slider as droppable
-sliderDiv.droppable({
-    //on drop 
-    drop: function (e, ui) {
-        // $("#drag-handle").fadeIn(900, function(){
-            // $("#drag-handle").css("background", "#020b11");
-            $("#drag-handle").css('opacity', '1.0');
-        // });
-        // $("#drag-handle").fadeTo(800, 1.0, function(){
-        //     $(this).css("background", "#020b11");
-        //     $(this).css('opacity', '1.0');
-        // });
-        if ($("#next-text").length){
-            $("#next-text").fadeIn("800", function(){
-                $("#next-text").css('opacity', '1.0');
-            });
-        }
+// sliderDiv.droppable({
+//     //on drop
+//     drop: function (e, ui) {
+//         $("#drag-handle").css('opacity', '1.0');
+//
+//         if ($("#next-text").length){
+//             $("#next-text").fadeIn("800", function(){
+//                 $("#next-text").css('opacity', '1.0');
+//             });
+//         }
+//
+//         var finalMidPosition = $(ui.draggable).position().top;
+//         resValue = Math.round((finalMidPosition - min + halfHandleHeight) / step);
+//         percentValue = linear(resValue, low, high, min, max);
+//
+//         sliderDiv.slider("value", resValue);
+//         console.log(resValue);
+//         console.log(percentValue);
+//
+//         if (percentValue <= 20) {
+//             result = option1;
+//         }
+//         else if (percentValue >= 21 && percentValue <= 50) {
+//             result = option2;
+//         }
+//         else if (percentValue >= 51 && percentValue <= 60) {
+//             result = option3;
+//         }
+//         else if (percentValue >= 61 && percentValue <= 80) {
+//             result = option4;
+//         }
+//         else if (percentValue >= 81 && percentValue <= 100) {
+//             result = option5;
+//         }
+//         //update result on page
+//         console.log(result + ": " + percentValue + "%");
+//
+//         $("#final_result").text(result);
+//     }
+// });
 
-        var finalMidPosition = $(ui.draggable).position().top;
-        resValue = Math.round((finalMidPosition - minY + halfHandleHeight) / tickSize);
-
-        sliderDiv.slider("value", resValue);
-
-        if (resValue <= 30) {
-            result = option1;
-        }
-        else if (resValue >= 21 && resValue <= 50) {
-            result = option2;
-        }
-        else if (resValue >= 51 && resValue <= 60) {
-            result = option3;
-        }
-        else if (resValue >= 61 && resValue <= 80) {
-            result = option4;
-        }
-        else if (resValue >= 81 && resValue <= 100) {
-            result = option5;
-        }
-        //update result on page
-        console.log(result + ": " + resValue + "%");
-
-        $("#final_result").text(result);
-    }
-});
+function linear(input, low, high, min, max){
+    return Math.ceil(min+(max-min)*(input-low)/(high-low));
+}
 
 
 function nextButton(){
