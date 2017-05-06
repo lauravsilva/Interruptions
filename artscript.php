439a89b6d6c62e33aa2fa0ead053edff1a9b6fb3
ToddWilliams459 <tkw8341@rit.edu>
2017-05-06 05:31:41 -0400

    Add files via upload

--- artscript.php
+++ artscript.php
@@ -1,9 +1,18 @@
 <?php
-$tmpfname = tempnam("assets/artpieces",$_POST['userKey1'] . $_POST['userKey2']);
+	require_once "assets/classes/DB.class.php";
+    require_once "assets/classes/util.php";
 
-$fp = fopen($tmpfname . ".png", "w");
+	$filename =  $_POST['userKey1'] . $_POST['userKey2'] . ".png";
+	
+	
+$fp = fopen('assets/artpieces/' . $filename, "w");
 fwrite($fp, base64_decode(explode(",", $_POST['data'])[1]));
 fclose($fp);
 unlink($tmpfname);
+
+$db = new DB();
+
+$db->updateArtPieceName($_POST['userKey1'], $filename);
+$db->updateArtPieceName($_POST['userKey2'], $filename);
 
 ?>
